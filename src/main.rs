4b1e93a045203fa9ae81fb59e67bbbf0f02beaea
Tiago Ribeiro <tribeiro@lsst.org>
2024-08-24 12:10:24 -0400

    Update main.rs.
    This is mostly a test program for the different submodules querying the different sources.
    
    This commit comments most of the tests and only leave the ones for EFD querying.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,24 @@
 mod exposure_log;
+mod fault_log;
 mod narrative_log;
 
 #[macro_use]
 extern crate serde_derive;
 
+use rolex::block_log::block_log::BlockLog;
+use rolex::night_plan::night_plan::NightPlan;
 use std::{collections::HashMap, error::Error};
 use url::Url;
 
-use crate::{exposure_log::exposure_log::ExposureLog, narrative_log::narrative_log::NarrativeLog};
+use crate::{
+    exposure_log::exposure_log::ExposureLog, fault_log::fault_log::FaultLog,
+    narrative_log::narrative_log::NarrativeLog,
+};
 use chrono;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    /*
     let base_url = "https://tucson-teststand.lsst.codes/exposurelog/messages";
     let mut url = Url::parse(base_url).unwrap();
 
@@ -59,10 +66,33 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let narrative_logs = NarrativeLog::retrieve(base_url, &params).await?;
     println!("Got {} entries: {:?}", narrative_logs.len(), narrative_logs);
 
+    // let base_url: &str = "https://api.zephyrscale.smartbear.com/v2/";
+    // let test_cycle_key: &str = "BLOCK-R19";
+
+    // let night_plan = NightPlan::retrieve(base_url, test_cycle_key).await?;
+    // println!("NightPlan = {night_plan:?}");
+
+    // let night_plan_status = night_plan.get_status().await?;
+    // println!("{night_plan_status}");
+
+    // let night_plan_owner = night_plan.get_owner().await;
+    // println!("{night_plan_owner:?}");
+
+    // let night_plan_links = night_plan.get_links().await?;
+    // println!("{night_plan_links}");
+    */
     let parse_from_str = chrono::NaiveDateTime::parse_from_str;
 
-    let date_start = parse_from_str("2024-01-18T12:00:00", "%Y-%m-%dT%H:%M:%S")?;
+    let date_start = parse_from_str("2024-08-13T12:00:00", "%Y-%m-%dT%H:%M:%S")?;
     let date_end = date_start + chrono::Duration::days(1);
     println!("{date_start:?} {date_end:?}");
+
+    let fault_logs = FaultLog::retrieve(&date_start, &date_end).await;
+
+    println!("{fault_logs:?}");
+
+    let block_logs = BlockLog::retrieve("summit_efd", &date_start, &date_end).await;
+
+    println!("{block_logs:?}");
     Ok(())
 }
